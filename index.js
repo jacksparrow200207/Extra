166ac642c146cf89ee7f3cd23b5707381613e352
jacksparrow200207 <zuhriddin02b@gmail.com>
2023-04-09 23:49:52 +0500

    solved problems

---
+++ index.js
@@ -0,0 +1,60 @@
+// let obj = ['array', 'object'];
+// let map = new Map([['name', 'border']])
+// let object = {name: 'Akobir'}
+
+// console.log(Object.fromEntries(object))
+
+// let list = [
+//   { id: 1, name: "Eshmat", title: "CEO" },
+//   { id: 2, name: "Toshmat", title: "CEO" },
+//   { id: 5, name: "Eshmat", title: "CTO" },
+//   { id: 6, name: "Toshmat", title: "CTO" },
+// ];
+
+// list.map((value) => {
+// 	for(let title in value) {
+//   	console.log(`${title}: ${value}`);}})
+
+// const sorts = (func) => {
+//   // res={}
+//   //  let res= list.map(title=>title.name[i]===title.name[i+1]? res+=())
+
+//   let list1 = list.title.map((value,i) =>
+//     value[i] === value[i+1] ? `"value.title":value` : value
+//   );
+
+//   return list1;
+
+//   // let list1 = list.filter((item) => item.title === "CEO");
+// let list2 = list.filter((item) => item.title === "CTO");
+// let res = {
+//   "ceo":list1,
+//   "cto":list2
+// }
+// ceo += list2
+// cto += list2
+//  return res
+// };
+// sorts();
+// console.log(sorts());
+
+let str = [-1, -5, -4, 0, 1, 5, 7, 8, 8, 4];
+
+// str.push(6)
+// console.log(str.slice(0,4))
+// // console.log(str.splice(0))
+// console.log(str.reverse())
+
+let arr = [
+  { id: 1, name: "zuhriddin", age: 23 },
+  { id: 2, name: "asilbek", age: 34 },
+  { id: 1, name: "Abdulahad", age: 23 },
+  { id: 2, name: "Mahmud", age: 32 },
+  { id: 1, name: "Luqmon", age: 13 },
+  { id: 2, name: "Alibek", age: 32 },
+];
+
+let num = 0;
+
+console.log(Math.sign(num));
+
